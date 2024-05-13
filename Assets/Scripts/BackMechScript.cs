c75b63dcacf548c4c7a4c42154c886e3623eaba4
Karmelica <damianniedziolka7@gmail.com>
2024-05-13 16:57:54 +0200

    Cores apply automatic

--- Assets/Scripts/BackMechScript.cs
+++ Assets/Scripts/BackMechScript.cs
@@ -5,58 +5,45 @@ using UnityEngine.UI;
 public class BackMechScript : MonoBehaviour
 {
     public Mech mechScript;
-    private RectTransform currentCore = null;
     public Button leftCoreButton;
     public Button rightCoreButton;
+    public GameObject leftCore;
+    public GameObject rightCore;
+    public GameObject fuelCore;
 
-    private IEnumerator coreManager(RectTransform current)
-    {
-        current.localScale = Vector3.one;
-        current.gameObject.GetComponent<Button>().enabled = false;
-        current.gameObject.GetComponent<Image>().color = Color.red;
-        yield return new WaitForSeconds(3f);
-        current.gameObject.GetComponent<Image>().color = Color.cyan;
-        current.localPosition = Vector3.zero;
-        current.localScale = Vector3.one * 0.7f;
-        current.gameObject.GetComponent<Button>().enabled = true;
-        current.gameObject.SetActive(false);
-    }
 
-    public void GetThisImage(RectTransform buttonCore)
-    {
-        currentCore = buttonCore;
-    }
+    //public void GetThisImage(RectTransform buttonCore)
+    //{
+    //    currentCore = buttonCore;
+    //}
 
-    public void PutInLeftCore(RectTransform coreTransform)
+    public void PutInLeftCore()
     {
-        if(currentCore != null)
+        if(mechScript.CoreChecker())
         {
             mechScript.LeftOverchargeDelete();
-            currentCore.position = coreTransform.position;
-            StartCoroutine(coreManager(currentCore));
-            Destroy(leftCoreButton.gameObject);
-            currentCore = null;
+            mechScript.CoreGiver().SetActive(false);
+            Destroy(leftCoreButton);
+            leftCore.SetActive(true);
         }
     }
-    public void PutInRightCore(RectTransform coreTransform)
+    public void PutInRightCore()
     {
-        if (currentCore != null)
+        if (mechScript.CoreChecker())
         {
             mechScript.RightOverchargeDelete();
-            currentCore.position = coreTransform.position;
-            StartCoroutine(coreManager(currentCore));
-            Destroy(rightCoreButton.gameObject);
-            currentCore = null;
+            mechScript.CoreGiver().SetActive(false);
+            Destroy(rightCoreButton);
+            rightCore.SetActive(true);
         }
     }
-    public void RefillFuel(RectTransform coreTransform)
+    public void RefillFuel()
     {
-        if (currentCore != null)
+        if (mechScript.CoreChecker() && !fuelCore.activeInHierarchy)
         {
-            currentCore.position = coreTransform.position;
-            StartCoroutine(coreManager(currentCore));
             mechScript.AddFuel();
-            currentCore = null;
+            mechScript.CoreGiver().SetActive(false);
+            fuelCore.SetActive(true);
         }
     }
 }
